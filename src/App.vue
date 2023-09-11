743dd4e51f5ab22c8541bf73d2db2cd180ec9f3e
ilham <ilhamyurest22@gmail.com>
2023-09-11 10:33:11 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader v-bind:TextHeader="textHeader" />
+  <input type="text" v-model="search" placeholder="Search..." />
+  <h2>Name: {{ name }} </h2>
+  <input type="text" v-model="newName"><br>
+  <button v-on:click="changeName">Change Name</button><br>
+  <a v-bind:href="url">Go To Google.com</a>
+
+  <div v-if="products.length">
+    <ul>
+      <!-- loop data -->
+      <li v-for="product in filterProducts" v-bind:key="product.id">
+        {{ product.title }} - {{ product.price }}
+      </li>
+    </ul>
+  </div>
+  <!-- jika tidak ada data -->
+  <div v-else>
+    <p>No Data</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from "./components/Header.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AppHeader,
+  },
+  data() {
+    return {
+      url: "https://google.com",
+      name: "Udin Arbain",
+      newName: "",
+      search: "",
+      products: [],
+      // textHeader : "Welcome Bambang"
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = this.newName
+    },
+  },
+  created() {
+    this.products = [
+      { id: 1, title: "Product 1", price: 5000 },
+      { id: 2, title: "Product 2", price: 4500 },
+      { id: 3, title: "Product 3", price: 3700 },
+      { id: 4, title: "Product 4", price: 1200 },
+    ];
+  },
+  // Computed Properties
+  computed: {
+    filterProducts() {
+      //      alert("DSDs")
+      // filter product dari array
+      return this.products.filter((product) => {
+        // Return title = search
+        return product.title.toUpperCase().match(this.search.toUpperCase());
+      });
+    },
+    textHeader() {
+      return `Welcome ${this.name}`;
+    },
+  },
 }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>{{ TextHeader }}</h2>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    props:["TextHeader"],
+};
+</script>
+
+<style></style>
